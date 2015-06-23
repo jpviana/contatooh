889fb3b17da71ba9ff3ab1db260874aa1a0ea874
Joao Paulo <joaopaulosis@hotmail.com>
2015-06-23 20:32:42 -0300

    deploy continuo

--- app/models/contato.js
+++ app/models/contato.js
@@ -21,3 +21,4 @@ module.exports = function() {
 
   return mongoose.model('Contato', schema);
 };
+
--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+var url = 'mongodb://localhost/contatooh';
+    if( process.env.OPENSHIFT_MONGODB_DB_PASSWORD ){
+        url =   "mongodb://" +
+                process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+                process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+                process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+                process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+                process.env.OPENSHIFT_APP_NAME;
+    }
+
 var http = require('http');
 var app  = require('./config/express')();
 require('./config/passport')();
-require('./config/database.js')('mongodb://localhost/contatooh');
+require('./config/database.js')(url);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(port, function(){
   console.log('Express Server escutando na porta ' + 
-              app.get('port'));
+              port);
 });
