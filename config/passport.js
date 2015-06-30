3de69fa70e34a7990549902df2d9d38b2c492ec0
João Paulo <joaopaulosis@hotmail.com>
2015-06-30 19:37:01 -0300

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -7,16 +7,11 @@ module.exports = function() {
 	
 	var Usuario = mongoose.model('Usuario');
 	
-	if (process.env.OPENSHIFT_NODEJS_PORT) {
+	
 	passport.use(new GitHubStrategy({
 		clientID: 'ae2ae332ca5550afd9cf',
 		clientSecret: '22a6551fcc5a746181375995062373dd437c04c1',
 		callbackURL: 'http://web-contatoooh.rhcloud.com/auth/github/callback'
-	}
-	passport.use(new GitHubStrategy({
-		clientID: 'ae2ae332ca5550afd9cf',
-		clientSecret: '22a6551fcc5a746181375995062373dd437c04c1',
-		callbackURL: 'http://localhost:3000/auth/github/callback'
 	}, function(accessToken, refreshToken, profile, done) {
 		
 		Usuario.findOrCreate(
